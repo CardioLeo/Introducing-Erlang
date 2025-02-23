c05a7f0c6af13f628045568db3eddd59c50d84ef
CardioLeo <CardioLeo@gmail.com>
2025-02-22 23:31:02 -0700

    fixes missing closing parentheses in the drop.erl file

--- 02-chapter/drop.erl
+++ 02-chapter/drop.erl
@@ -1,6 +1,6 @@
 -module(drop).
 -export([fall_velocity/1]).
 
--spec(fall_velocity(number() -> number()).
+-spec(fall_velocity(number()) -> number()).
 
 fall_velocity(Distance) -> math:sqrt(2 * 9.8 * Distance).
